e2ba423271f3554aba40b91bf7937f52b037f929
a8-TH-I100-鄭壬乙 <chengry@THCI.COM.TW>
2020-03-31 12:07:06 +0800

    build

---
+++ client/build/precache-manifest.e70bd67742d7ee5b0b62feb8ccc7ebcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f93d27c9efc506ac4e6421087ab98d1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "40e3c5bbd28a0e3f2eb1",
+    "url": "/static/css/main.2b30fe85.chunk.css"
+  },
+  {
+    "revision": "af02f1e2d53d4f95d453",
+    "url": "/static/js/2.4ff82786.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4ff82786.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40e3c5bbd28a0e3f2eb1",
+    "url": "/static/js/main.95a557f1.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  }
+]);
