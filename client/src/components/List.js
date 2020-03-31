313ca5416f7a5210de7064a6449d571c1282f2ae
a8-TH-I100-鄭壬乙 <chengry@THCI.COM.TW>
2020-03-31 11:57:16 +0800

    select open end close

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -7,7 +7,8 @@ const List = ({ todos, deleteTodo }) => {
     }, []);
 
     const [data, setData] = useState([]);
-    const [search, setSearch] = useState([]);
+    const [search, setSearch] = useState("");
+    const [select, setSelect] = useState("Open");
     const getList = () => {
         axios
             .get("/api/test")
@@ -21,10 +22,24 @@ const List = ({ todos, deleteTodo }) => {
     const updateSearch = value => {
         setSearch(value);
     };
+    const updateSelect = value => {
+        setSelect(value);
+    };
     const filterData = () => {
-        return data.filter(item => {
+        const filter1 = data.filter(item => {
             return item.title.indexOf(search) !== -1;
         });
+        let filter2;
+        if (select == "Closed") {
+            filter2 = filter1.filter(item => {
+                return item[day_list[day].day[1]].indexOf(select) !== -1;
+            });
+        } else {
+            filter2 = filter1.filter(item => {
+                return item[day_list[day].day[1]].indexOf(select) == -1;
+            });
+        }
+        return filter2;
     };
     const day_list = [
         { day: ["日", "SUN"] },
@@ -46,6 +61,10 @@ const List = ({ todos, deleteTodo }) => {
                 placeholder="查詢店家名稱"
                 onChange={e => updateSearch(e.target.value)}
             />
+            <select onChange={e => updateSelect(e.target.value)}>
+                <option value="Open">Open</option>
+                <option value="Closed">Closed</option>
+            </select>
             <ul className="list">
                 {filterData() && filterData().length > 0 ? (
                     filterData().map(item => {
